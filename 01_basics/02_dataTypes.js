11eac3afc98c1ce5bd30e8bf507215d0322333e9
pratik-kharva27 <pratikkharva01@gmail.com>
2024-02-29 15:46:29 +0530

    datatype discussion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+
+let name = "sparrow"
+let age = 18
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+// bigint
+// string =>''
+// boolean => true/ false
+// null => standalone value
+// undefined => 
+// symbol => find unique ness 
+
+//object 
+
+console.log(typeof "sparrow");
+console.log(typeof null);
+console.log(typeof undefined);
