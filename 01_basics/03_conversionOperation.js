27d81de88d4855530ad4fa912d473c6b3b5495af
pratik-kharva27 <pratikkharva01@gmail.com>
2024-02-29 16:51:18 +0530

    Operation discussion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,14 @@
 let score =33
 
-console.log(typeof(score));
-console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof score);
 
 let num = "333abc"
 let valueInNumber = Number(num)
- console.log(typeof valueInNumber);
+//  console.log(typeof valueInNumber);
 
  let x = null
- console.log(typeof x, "sparrow");
+//  console.log(typeof x, "sparrow");
 
 
  //"33"=> 33
@@ -16,9 +16,53 @@ let valueInNumber = Number(num)
  // true => 1; false =>0
  let isLoggedIn = 1
   let booleanIsLoggedIn = Boolean(isLoggedIn)
-  console.log((booleanIsLoggedIn));
+//   console.log((booleanIsLoggedIn));
 
 // in to boolean
   // 1 => true; 0=>false
   // "" => false
   // "sparrow" => true
+
+  //************************** Operation *******************************
+
+  let value =3
+  let negvalue = -value
+
+//   console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "sparrow"
+let str2 = "pratik"
+let str3 = str1 + str2;
+// console.log(str1,str2);
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3;
+ num1 = num2 = num3 = 2+2;
+
+ console.log(num2);
+
+ let gameCounter = 100;
+ ++ gameCounter;
+ gameCounter++;
+ --gameCounter;
+ gameCounter--;
+ console.log(gameCounter);
