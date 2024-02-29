e50648c8eb4c63c7643b4bceeb2c37be8a70c782
pratik-kharva27 <pratikkharva01@gmail.com>
2024-02-29 16:07:03 +0530

    conversion discussion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score =33
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let num = "333abc"
+let valueInNumber = Number(num)
+ console.log(typeof valueInNumber);
+
+ let x = null
+ console.log(typeof x, "sparrow");
+
+
+ //"33"=> 33
+ // "333abc" => NaN
+ // true => 1; false =>0
+ let isLoggedIn = 1
+  let booleanIsLoggedIn = Boolean(isLoggedIn)
+  console.log((booleanIsLoggedIn));
+
+// in to boolean
+  // 1 => true; 0=>false
+  // "" => false
+  // "sparrow" => true
