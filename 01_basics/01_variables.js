ba069e3f29dcf669612452d4409fc8c90b79c383
pratik-kharva27 <pratikkharva01@gmail.com>
2024-02-29 15:46:00 +0530

    variables discussion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "sparrow@gmail.com"
+var accountPassword = "12345"
+accountCity = "vadodara"
+
+// accountId = 2 //not allowed 
+
+
+
+console.log(accountId,"sparrow");
+
+
+
