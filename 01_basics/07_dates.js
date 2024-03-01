13de0373093f44ac1856ab9f8fdf8d2b9471a97a
pratik-kharva27 <pratikkharva01@gmail.com>
2024-03-01 17:04:33 +0530

    Date and Time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,26 @@
+// Dates and Times
+
+const myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date(2023,0,23) // start with array
+// let myCreatedDate = new Date("2023-01-14") // yyyy/mm/dd
+let myCreatedDate = new Date("01-14-2023") // mm/dd/yyyy
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
