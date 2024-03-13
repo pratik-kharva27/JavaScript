92f66a1a2c26a3c23cabd9340019526c3f7e26a4
pratik-kharva27 <pratikkharva01@gmail.com>
2024-03-13 16:19:24 +0530

    filter with array

---
+++ 05_iterations/six.js
@@ -0,0 +1,31 @@
+// for each loop not retrun any value
+
+// const coding = ["js", "cpp", "rb", "swift"];
+// const values = coding.forEach((item)=>{
+//     console.log(item);
+//     return item
+// })
+// console.log(values);
+
+
+// filter take callback function 
+// filter return value
+//filter condition return true of false 
+
+// const myNums = [1,2,3,4,5,6,7,8,9]
+// const newNum = myNums.filter((num)=> num > 4)
+
+//second way
+// const newNum = myNums.filter((num)=> { return  num > 4 })
+// console.log(newNum);
+
+
+// //with foreach same example 
+// const newNum = []
+// myNums.forEach((num)=>{
+//     if(num > 4)
+//         return newNum.push(num)
+// })
+// console.log(newNum);
+
+
