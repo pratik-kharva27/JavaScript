5d70dd2b97ed338d857d54c5d1fab7232b4c6841
pratik-kharva27 <pratikkharva01@gmail.com>
2024-03-13 16:12:35 +0530

    for loop discussion

---
+++ 05_iterations/one.js
@@ -0,0 +1,14 @@
+//for
+const array_length = 10;
+
+for (let i = 0; i < array_length; i++) {
+    console.log(`${i}`); 
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(`${i}`);
+    for (let j = 0; j < 10; j++) {
+        console.log(`${j}  and ${i}`);
+        
+    }
+}
