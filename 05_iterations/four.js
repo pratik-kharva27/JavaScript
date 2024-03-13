64823cf3d4c82cf52050f68e4ca2e177ec76ecdd
pratik-kharva27 <pratikkharva01@gmail.com>
2024-03-13 16:17:14 +0530

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,25 @@
+// for in loop 
+
+const myobj = {
+    js :'javascript',
+    cpp :'c++',
+    rb : 'ruby',
+    swift :'swift by apple'
+};
+
+for(const key in myobj){
+    // console.log(key);
+    // console.log(myobj[key]);
+    // console.log(`this is key ${key} and this is value ${myobj[key]}`);
+    // console.log(myobj);
+}
+
+
+const programming = ["js","cpp","rb","swift"]
+
+for (const key in programming){
+    // console.log(`this is key ${key} : ${programming[key]}`);
+}
+
+
+
